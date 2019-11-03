7922e2ea395cdf1ff57971f1de2c38fd8ddce389
pawlaczyk <pawlaczyk@g.pl>
2019-11-03 21:19:52 +0100

    testowe gorne menu

---
+++ AssessmentTest/frontend/src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav>
+        <v-toolbar flat app>
+        <v-toolbar-title class="text-uppercase grey--text">
+            <span class="font-weight-light">Ocenianie</span>
+            <span>Testów</span>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn flat color="grey">
+            <span>Wyloguj</span>
+            <!-- <v-icon>{{ clear }}</v-icon> -->
+        </v-btn>
+        </v-toolbar>
+    </nav>
+</template>
